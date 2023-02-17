2af1907b0f7264221e9fc386712c48cb93041990
Nadia Filatova <nadiakocheluk09@gmail.com>
2023-02-17 23:02:41 +0200

    Initial commit

---
+++ src/homework/Car.java
@@ -0,0 +1,20 @@
+package homework;
+
+public class Car {
+    private int year;
+    private String color;
+
+    public Car() {
+
+    }
+
+    public Car(int year) {
+        this.year = year;
+
+    }
+
+    public Car(int year, String color) {
+        this.year = year;
+        this.color = color;
+    }
+}
---
+++ src/homework/Main.java
@@ -0,0 +1,11 @@
+package homework;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car car1= new Car();
+        Car car2 =new Car(1999);
+        Car car3 = new Car(2002, "white");
+
+    }
+}
